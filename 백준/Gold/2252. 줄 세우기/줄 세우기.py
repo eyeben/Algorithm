9ed92e252dc55877410a0d59c77c55be8a7ca558
DongMin <62013201+eyeben@users.noreply.github.com>
2024-09-08 15:01:40 +0900

    [Gold III] Title: 줄 세우기, Time: 2492 ms, Memory: 38744 KB -BaekjoonHub

---
+++ 백준/Gold/2252. 줄 세우기/줄 세우기.py
@@ -0,0 +1,28 @@
+from collections import deque
+N, M = map(int, input().split())
+
+indegree = [0]* (N+1)
+adjs = [[] for _ in range(N+1)]
+
+
+for _ in range(M):
+    a,b = map(int, input().split())
+    adjs[a].append(b)
+    indegree[b] += 1
+
+q = deque()
+
+for i in range(1,N+1):
+    if indegree[i] == 0:
+        q.append(i)
+
+ans = []
+while q:
+    node = q.popleft()
+    ans.append(node)
+    for itm in adjs[node]:
+        indegree[itm] -= 1
+        if indegree[itm] == 0:
+            q.append(itm)
+
+print(*ans)
