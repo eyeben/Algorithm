c2b2e4bb3e36117a03827c318a3cc9e53c489065
DongMin <62013201+eyeben@users.noreply.github.com>
2024-12-12 00:59:46 +0900

    [Gold III] Title: 사다리 조작, Time: 2588 ms, Memory: 432972 KB -BaekjoonHub

---
+++ 백준/Gold/15684. 사다리 조작/사다리 조작.py
@@ -0,0 +1,50 @@
+from itertools import combinations
+N, M, H = map(int, input().split())
+li = [[False] * (N-1) for _ in range(H)]
+for i in range(M):
+    a,b = map(int,input().split())
+    li[a-1][b-1] = True
+
+# end = [False] * N
+def draw(n):
+    nowAt = n
+    for i in range(H):
+        if nowAt -1 >=0 and li[i][nowAt-1]:
+            nowAt -= 1
+        elif nowAt +1<N and li[i][nowAt]:
+            nowAt += 1
+    return nowAt == n
+    # end[n] = True
+
+
+# for i in range(N):
+#     draw(i)
+
+all = []
+for i in range(N-1):
+    for j in range(H):
+        if not li[j][i]:
+            all.append((i,j))
+combi = []
+for i in range(4):
+    combi += list(combinations(all,i))
+for itms in combi:
+
+    for x,y in itms:
+        li[y][x] = True
+    goodFlag = True
+    for i in range(N):
+        if not draw(i):
+            goodFlag = False
+            break
+
+    if goodFlag:
+        print(len(itms))
+        exit()
+    for x,y in itms:
+        li[y][x] = False
+print(-1)
+
+
+def dfs(level, x, y):
+    pass
