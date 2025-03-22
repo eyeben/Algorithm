1dcd54ab4430255c864257315a7ccee175f00701
DongMin <62013201+eyeben@users.noreply.github.com>
2025-03-22 20:55:53 +0900

    [Gold II] Title: 친구 네트워크, Time: 3320 ms, Memory: 178044 KB -BaekjoonHub

---
+++ 백준/Gold/4195. 친구 네트워크/친구 네트워크.py
@@ -0,0 +1,33 @@
+def findParent(n):
+    if parent[n] != n:
+        parent[n] = findParent(parent[n])
+    return parent[n]
+
+
+def union(a,b):
+    a = findParent(a)
+    b = findParent(b)
+    if a < b:
+        parent[a] = b
+        cntDict[a] = cntDict[b] = cntDict[a] + cntDict[b]
+    elif a > b:
+        parent[b] = a
+        cntDict[a] = cntDict[b] = cntDict[a] + cntDict[b]
+
+
+for i in range(int(input())):
+    N = int(input())
+    parent = dict()
+    cntDict = dict()
+    for _ in range(N):
+        a, b = input().split()
+        if a not in parent:
+            parent[a] = a
+            cntDict[a] = 1
+        if b not in parent:
+            parent[b] = b
+            cntDict[b] = 1
+        if findParent(a) != findParent(b):
+            union(a, b)
+
+        print(cntDict[findParent(a)])
