929db2a237b459f2ba0526f12cf80bd14d95e40f
DongMin <62013201+eyeben@users.noreply.github.com>
2025-05-18 05:38:43 +0900

    [Gold III] Title: 크게 만들기, Time: 576 ms, Memory: 56136 KB -BaekjoonHub

--- 백준/Gold/2812. 크게 만들기/크게 만들기.py
+++ 백준/Gold/2812. 크게 만들기/크게 만들기.py
@@ -18,11 +18,9 @@ for i in range(K, N):
     for j in range(9, -1, -1):
         if dic[j]:
             idx = heapq.heappop(dic[j])
-            ans1.append((j, idx))
+            ans1.append(str(j))
             for k in range(head, idx):
                 heapq.heappop(dic[li[k]])
             head = idx + 1
             break
-ans1.sort(key=lambda x: x[1])
-ans = [str(itm[0]) for itm in ans1]
-print(''.join(ans))
+print(''.join(ans1))
