7723bf6a9621c6b9454689cb69d9dbb123374e77
DongMin <62013201+eyeben@users.noreply.github.com>
2025-06-01 03:24:30 +0900

    [Silver I] Title: 돌다리, Time: 152 ms, Memory: 114976 KB -BaekjoonHub

--- 백준/Silver/12761. 돌다리/돌다리.py
+++ 백준/Silver/12761. 돌다리/돌다리.py
@@ -1,18 +1,18 @@
-import heapq
+from collections import deque
 A, B, N, M = map(int,input().split())
 dol = [100_001] * 100_001
 dol[N] = 0
-hq = [(0, N)]
-while hq:
-    dist, n = heapq.heappop(hq)
+dq = deque([(0, N)])
+while dq:
+    dist, n = dq.popleft()
     for i in (-1, 1, -A, A, -B, B):
         if 0 <= n + i <= 100_000 and (dist + 1 < dol[n + i]):
             dol[n + i] = dist + 1
-            heapq.heappush(hq, (dol[n + i], n + i))
+            dq.append((dol[n + i], n + i))
 
     for i in (A, B):
         if 0 <= n * i <= 100_000 and dist + 1 < dol[n * i]:
             dol[n * i] = dist + 1
-            heapq.heappush(hq, (dol[n * i], n * i))
+            dq.append((dol[n * i], n * i))
 
 print(dol[M])
