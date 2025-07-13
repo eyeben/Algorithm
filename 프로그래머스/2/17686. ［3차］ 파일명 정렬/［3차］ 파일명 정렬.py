371ac0c3d9e00c9b09a7b94fb5383e5b65552b22
DongMin <62013201+eyeben@users.noreply.github.com>
2025-07-13 10:22:24 +0900

    [level 2] Title: [3차] 파일명 정렬, Time: 3.55 ms, Memory: 9.73 MB -BaekjoonHub

--- 프로그래머스/2/17686. ［3차］ 파일명 정렬/［3차］ 파일명 정렬.py
+++ 프로그래머스/2/17686. ［3차］ 파일명 정렬/［3차］ 파일명 정렬.py
@@ -1,26 +1,23 @@
 def solution(files):
     words = []
+    nums = {'0','1','2','3','4','5','6','7','8','9'}
     for i in range(len(files)):
         word = files[i]
         idx1 = 0
         idx2 = 0
         for j in range(len(word)):
-            if word[j] in ('0','1','2','3','4','5','6','7','8','9'):
+            if word[j] in nums:
                 idx1 = idx2 = j
                 for k in range(j, len(word)):
-                    if word[k] not in ('0','1','2','3','4','5','6','7','8','9'):
+                    if word[k] not in nums:
                         break
                     idx2 = k
-                    
                 break
         tmp = [word[:idx1], word[idx1:idx2+1]]
         words.append([tmp, word])
+        
     words.sort(key = lambda x : int(x[0][1]))
     words.sort(key = lambda x : x[0][0].upper())
 
-    print(words)
+    return [itm[1] for itm in words]
     
-    
-    
-    answer = [itm[1] for itm in words]
-    return answer
