488637a0f14c148d16fa67573699734c519643df
DongMin <62013201+eyeben@users.noreply.github.com>
2025-08-10 02:32:58 +0900

    [Gold III] Title: 욕심쟁이 판다, Time: 768 ms, Memory: 54452 KB -BaekjoonHub

--- 백준/Gold/1937. 욕심쟁이 판다/욕심쟁이 판다.py
+++ 백준/Gold/1937. 욕심쟁이 판다/욕심쟁이 판다.py
@@ -1,34 +1,31 @@
 import sys
-input = sys.stdin.readline
 sys.setrecursionlimit(10**4*25)
 
 N = int(input())
-trees = [list(map(int, input().split())) for _ in range(N)]
+li = [list(map(int, input().split())) for _ in range(N)]
 
-cache = [[-1] * N for _ in range(N)]
+dx = [0, 1, 0, -1]
+dy = [1, 0, -1, 0]
+visited = [[False] * N for _ in range(N)]
 
-dy = [-1, 1, 0, 0]
-dx = [0, 0, -1, 1]
+cache = [[0 for _ in range(N)] for _ in range(N)]
 
-def dfs(x,y):
-    # 방문한 노드면 그냥 리턴
-    if cache[y][x] != -1: # 방문한 노드는 최대 값을 보장하는가? yee
-        return cache[y][x]
 
-    mx = 1
-    for i in range(4):
-        nx,ny = x+dx[i], y+dy[i]
-        if not (0 <= nx < N and 0 <= ny < N):
-            continue
-        if trees[y][x] < trees[ny][nx]:
-            mx = max(mx, 1+ dfs(nx,ny))
+def dfs(x, y):
+    if not visited[y][x]:
+        num = 0
+        for i in range(4):
+            nx, ny = x + dx[i], y + dy[i]
+            if 0 <= nx < N and 0 <= ny < N and li[y][x] < li[ny][nx]:
+                num = max(num, dfs(nx, ny))
+        cache[y][x] = num + 1
+        visited[y][x] = True
+    return cache[y][x]
 
-    cache[y][x] = mx # 여기가 DP
-    return mx
 
-ans = 0
+mx = 0
 for i in range(N):
     for j in range(N):
-        ans = max(ans, dfs(i,j))
+        mx = max(mx, dfs(i, j))
 
-print(ans)
+print(mx)
