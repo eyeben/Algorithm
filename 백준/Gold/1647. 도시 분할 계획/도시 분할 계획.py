f1b6225a00f5f3cbf9bc2e9343fe13b084647e8b
DongMin <62013201+eyeben@users.noreply.github.com>
2025-11-23 20:39:43 +0900

    [Gold IV] Title: 도시 분할 계획, Time: 2168 ms, Memory: 269468 KB -BaekjoonHub

--- 백준/Gold/1647. 도시 분할 계획/도시 분할 계획.py
+++ 백준/Gold/1647. 도시 분할 계획/도시 분할 계획.py
@@ -2,36 +2,31 @@ import sys
 input = sys.stdin.readline
 
 N, M = map(int, input().split())
-li = []
+li = [list(map(int, input().split())) for _ in range(M)]
 
-for _ in range(M):
-    a,b,c = map(int, input().split())
-    a -= 1
-    b -= 1
-    li.append((c,a,b))
 
-parents = [i for i in range(N)]
-def unionFind(n):
-    if parents[n] != n:
-        parents[n] = unionFind(parents[n])
+parents = [i for i in range(N + 1)]
+
+def get_parent(n):
+    if n != parents[n]:
+        parents[n] = get_parent(parents[n])
     return parents[n]
 
-def union(n1,n2):
-    n1 = unionFind(n1)
-    n2 = unionFind(n2)
-    if n1 < n2:
-        parents[n1] = parents[n2]
-    elif n2 < n1:
-        parents[n2] = parents[n1]
+def union(a, b):
+    a = get_parent(a)
+    b = get_parent(b)
+    if a < b:
+        parents[b] = a
+    elif b < a:
+        parents[a] = b
 
-anss = []
-li.sort()
-for c,a,b in li:
-    if unionFind(a) == unionFind(b):
-        continue
-    union(a,b)
-    anss.append(c)
-    if len(anss) == N-1:
-        break
+li.sort(key = lambda x: x[-1])
+ans = 0
+last = 0
+for a,b,c in li:
+    if get_parent(a) != get_parent(b):
+        union(a, b)
+        ans += c
+        last = c
 
-print(sum(anss) - max(anss))
+print(ans - last)
